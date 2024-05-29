cdad842d290f7a0006d2121ed5695184cae1ee39
Facu-J-Boy <boyfacundo45@gmail.com>
2024-05-29 17:31:36 -0300

    conductor puede aceptar o rechazar ordenes

--- src/router/application.route.ts
+++ src/router/application.route.ts
@@ -4,10 +4,19 @@ import { ApplicationServcie } from '../services';
 
 const router = Router();
 //Servicios
-const { applyForOrder, assignDriverToOrder } = ApplicationServcie;
+const {
+  applyForOrder,
+  assignDriverToOrder,
+  declineOrder,
+  aceptOrder,
+} = ApplicationServcie;
 
 router.post('/apply', applyForOrder);
 
 router.post('/assign', assignDriverToOrder);
+
+router.patch('/decline_order/:orderId', declineOrder);
+
+router.patch('/acept_order/:orderId', aceptOrder);
 
 module.exports = router;
--- src/services/application.service.ts
+++ src/services/application.service.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express';
 import { ApplicationModel, OrderModel } from '../models';
+import { OrderStatus } from '../models/orders.model';
 
 const applyForOrder = async (req: Request, res: Response) => {
   try {
     const { driverId, orderId } = req.body;
+    const order = await OrderModel.findByPk(orderId);
+    if (!order) {
+      return res.status(404).json({ msg: 'Orden no encontrada' });
+    }
+    if (order.assignedDriverId || order.pendingAssignedDriverId) {
+      return res
+        .status(400)
+        .json({ msg: 'Esta orden ya tiene un conductor asignado' });
+    }
     const application = await ApplicationModel.create({
       driverId,
       orderId,
@@ -23,13 +33,52 @@ const assignDriverToOrder = async (req: Request, res: Response) => {
       return res.status(404).json({ error: 'Order not found' });
     }
 
-    order.assignedDriverId = driverId;
+    order.pendingAssignedDriverId = driverId;
     await order.save();
 
-    res.status(200).json(order);
+    res
+      .status(200)
+      .json({ msg: 'Conductor asignado a orden con exito', order });
   } catch (error) {
     res.status(500).send(error);
   }
 };
 
-export default { applyForOrder, assignDriverToOrder };
+const declineOrder = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const order = await OrderModel.findByPk(orderId);
+    if (!order) {
+      return res.status(404).json({ msg: 'Orden no encontrada' });
+    }
+    order.pendingAssignedDriverId = null;
+    await order.save();
+    res.status(200).json({ msg: 'Orden rechazada' });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const aceptOrder = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const order = await OrderModel.findByPk(orderId);
+    if (!order) {
+      return res.status(404).json({ msg: 'Orden no encontrada' });
+    }
+    order.assignedDriverId = order.pendingAssignedDriverId;
+    order.pendingAssignedDriverId = null;
+    order.status = OrderStatus.ASIGNADO;
+    await order.save();
+    res.status(200).json({ msg: 'Orden aceptada' });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export default {
+  applyForOrder,
+  assignDriverToOrder,
+  declineOrder,
+  aceptOrder,
+};
