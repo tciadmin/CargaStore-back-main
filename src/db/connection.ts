b9966a084f0aba5552d83f55ea685205da34e373
Facu-J-Boy <boyfacundo45@gmail.com>
2024-06-06 18:49:57 -0300

    cambios para pasar a producción

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -13,16 +13,46 @@ import {
 } from '../models';
 import EmailCodes from '../models/emailCodes.model';
 import PasswordCodes from '../models/passwordCodes.model';
-const { nameDB, userDB, PasswordDB, hostDB, portDB } = Config;
 
-const db = new Sequelize({
-  database: nameDB,
-  username: userDB,
-  password: PasswordDB,
-  host: hostDB,
+//DESARROLLO
+/////////////////////////////////////////////////////////
+
+// const { nameDB, userDB, PasswordDB, hostDB, portDB } = Config;
+
+// const db = new Sequelize({
+//   database: nameDB,
+//   username: userDB,
+//   password: PasswordDB,
+//   host: hostDB,
+//   dialect: 'mysql',
+//   logging: false,
+//   port: +portDB,
+//   timezone: '-05:00',
+//   models: [
+//     EmailCodes,
+//     PasswordCodes,
+//     UserModel,
+//     DriverModel,
+//     CustomerModel,
+//     OrderModel,
+//     PackageModel,
+//     TruckModel,
+//     ApplicationModel,
+//     FeedbackModel,
+//     PayModel,
+//   ], // Aquí añades tus modelos
+// });
+
+////////////////////////////////////////////////////////////
+
+//PRODUCCION
+////////////////////////////////////////////////////////////
+
+const { urlDB } = Config;
+
+const db = new Sequelize(`${urlDB}`, {
   dialect: 'mysql',
   logging: false,
-  port: +portDB,
   timezone: '-05:00',
   models: [
     EmailCodes,
@@ -38,6 +68,8 @@ const db = new Sequelize({
     PayModel,
   ], // Aquí añades tus modelos
 });
+
+/////////////////////////////////////////////////////////
 
 db.sync({ alter: true });
 
