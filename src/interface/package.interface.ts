d355c80c2cc25ff0a87ba23621ddb95f186f89fe
Facu-J-Boy <boyfacundo45@gmail.com>
2024-06-14 20:17:37 -0300

    packages images

--- src/interface/package.interface.ts
+++ src/interface/package.interface.ts
@@ -1,11 +1,14 @@
 export interface PackageInterface {
   id?: string;
   product_name: string;
+  image1?: string | null;
+  image2?: string | null;
+  image3?: string | null;
+  image4?: string | null;
   quantity: number;
   type: 'seca' | 'peligrosa' | 'refrigerada';
   weight: number;
   volume: number;
   offered_price: number;
-  product_pic: string;
   orderId?: string;
 }
