ca564ae844a69bbbfc976cd48fb0868b09d69aca
Facu-J-Boy <boyfacundo45@gmail.com>
2024-08-04 21:27:16 -0300

    mejoras en patchUser

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import CustomerModel from '../models/customers.model';
 import DriverModel from '../models/drivers.model';
 import { UserModel } from '../models';
-import { isMulterSingleFile } from '../config/multerConfig';
+// import { isMulterSingleFile } from '../config/multerConfig';
 import fs from 'fs';
 
 const getAllCustomersAndDrivers = async (
@@ -48,69 +48,73 @@ const getAllCustomersAndDrivers = async (
   }
 };
 const patchUser = async (req: Request, res: Response) => {
-  const userId = Array.isArray(req.headers.id)
-    ? req.headers.id[0]
-    : req.headers.id;
-  const {
-    name, // string
-    lastname, // string
-  } = req.body;
+  const { userId } = req.params;
+  const { name, lastname } = req.body;
   const regex = /^[a-zA-Z\s]+$/;
   const user = await UserModel.findByPk(userId);
 
-  if (!isMulterSingleFile(req.file)) {
-    console.log('error al subir los archivos: ', req.file);
-    return res
-      .status(400)
-      .json({ msg: 'Error al subir los archivos' });
-  }
-
-  if (user?.profile_image) {
-    fs.unlinkSync(user?.profile_image);
-  }
-
-  const file = req.file?.path;
-
-  if (!regex.test(name)) {
+  if (!regex.test(name) || !name) {
+    console.log(
+      'El nombre no puede contener números ni carácteres inválidos o estar vacío'
+    );
     return res.status(400).json({
-      msg: 'El nombre no puede contener números ni carácteres inválidos',
+      message: {
+        type: 'error',
+        msg: 'El nombre no puede contener números ni carácteres inválidos o estar vacío',
+      },
     });
-  } else if (!name) {
-    return res
-      .status(400)
-      .json({ msg: 'El nombre no puede estar vacío' });
-  } else if (!regex.test(lastname)) {
+  }
+  if (!regex.test(lastname) || !lastname) {
+    console.log(
+      'El apellido no puede contener números ni carácteres inválidos o estar vacío'
+    );
     return res.status(400).json({
-      msg: 'El apellido no puede contener números ni carácteres inválidos',
+      message: {
+        type: 'error',
+        msg: 'El apellido no puede contener números ni carácteres inválidos o estar vacío',
+      },
     });
-  } else if (!lastname) {
-    return res
-      .status(400)
-      .json({ msg: 'El apellido no puede estar vacío' });
   }
 
   try {
-    const [updated] = await UserModel.update(
-      { lastname, name, profile_image: file },
-      {
-        where: { id: userId },
+    let profileImageUpdated = false;
+    if (req.file) {
+      if (user?.profile_image) {
+        fs.unlinkSync(user.profile_image);
       }
+      await UserModel.update(
+        { profile_image: req.file.path },
+        { where: { id: userId } }
+      );
+
+      profileImageUpdated = true;
+    }
+
+    const [updated] = await UserModel.update(
+      { lastname, name },
+      { where: { id: userId } }
     );
 
-    if (updated) {
+    if (updated || profileImageUpdated) {
       const updatedUser = await UserModel.findOne({
         where: { id: userId },
       });
       console.log('User updated successfully:', updatedUser);
       return res.status(200).json({
-        msg: 'Se actualizar los datos del usuario con éxito',
+        message: {
+          type: 'success',
+          msg: 'Datos del usuario actualizado',
+        },
+        updatedUser,
       });
     }
+    console.log('usuario no pudo actualizarse');
 
     return res
       .status(400)
       .json({ msg: 'Usuario no pudo actualizarse' });
   } catch (error) {
+    console.log('ERROR: ', error);
     return res.status(500).json({ msg: 'Error interno' });
   }
 };
