5a40bbb694bb08420f8244af96dcf9da71afada8
tciadmin <proyectos@transcomerinter.com>
2025-04-22 12:40:46 -0500

    Actualizacion campos marca y modelo

--- src/router/truck.route.ts
+++ src/router/truck.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { TruckService } from '../services';
+import { vehicleData } from '../config/vehicleData';
 // import ValidJWT from "../middlewares/valid-jwt";
 
 const router = Router();
@@ -16,5 +17,22 @@ router.get(
   // ValidJWT,
   getAllTrucks
 );
+// Rutas para marcas de vehículos
+router.get('/vehicle/brands', (req, res) => {
+  const brands = Object.keys(vehicleData.marcas);  // Las claves son las marcas
+  res.json(brands);  // Devuelve las marcas de vehículos
+});
+
+// Rutas para modelos de vehículos
+router.get('/vehicle/models', (req, res) => {
+  const { brand } = req.query;  // Obtén la marca de la query
+
+  // Asegúrate de que 'brand' sea una clave válida dentro de 'vehicleData.marcas'
+  if (brand && vehicleData.marcas[brand as string]) {
+    res.json(vehicleData.marcas[brand as string]);  // Devuelve los modelos correspondientes
+  } else {
+    res.status(400).json({ error: 'Marca no válida' });
+  }
+});
 
 module.exports = router;
